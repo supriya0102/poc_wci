a59261992391764e51fc591693d1b602fee862ce
mahant.harbingergroup <v-mahant.gawhade@harbingergroup.com>
2023-02-28 00:23:00 +0530

    added api for landing page using redux

--- src/constant/Index.js
+++ src/constant/Index.js
@@ -1,4 +1,4 @@
-export const URL='https://dummyjson.com';
+export const URL='http://localhost:3000';
 export const USER_LIST_API_REQUEST = "USER_LIST_API_REQUEST";
 export const USER_LIST_API_SUCCESS = "USER_LIST_API_SUCCESS";
 export const USER_LIST_API_FAILURE = "USER_LIST_API_FAILURE";
