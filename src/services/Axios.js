8c57c72289358d87eef9e0b2e293071b54c6f1dc
AvinashGavandhe <avinash.gavandhe@harbingergroup.com>
2023-03-03 12:10:53 +0530

    id passed in url of profile page

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { URL } from '../constant/Index';
 
-// const {id} = useParams();
-
 
 export const userList = async () => axios.get(`${URL}/api/users`)
 export const viewUser = async (id) => axios.get(`${URL}/api/user/${id}`)
