d1a32be2cf70a9e29048f2cd534d7c014dec7482
AvinashGavandhe <avinash.gavandhe@harbingergroup.com>
2023-03-08 09:29:28 +0530

    cardView landing page added

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,31 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Header from "../header/Header";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Container,
-  IconButton,
-} from "@mui/material";
+import { IconButton, Avatar, TextField, Select, MenuItem } from "@mui/material";
 import { Edit } from "@mui/icons-material";
 import { users } from "../../store/home/HomeAction";
-import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import { useNavigate, Link } from "react-router-dom";
-
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import RestoreFromTrashIcon from "@mui/icons-material/RestoreFromTrash";
+import "./home.css";
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
 
 function Home() {
+  const [state, setState] = useState("");
   const dispatch = useDispatch();
   const reduxStore = useSelector((state) => state.usersReducer.users);
   const navigate = useNavigate();
+  
+
+  const colors = ["red", "green", "blue", "orange", "orange"];
+
+  const rgb = colors[Math.floor(Math.random() * colors.length)];
 
   const onViewClick = () => {
     navigate(`/profile/${reduxStore[0].id}`);
-    
+  };
+  const handleChange = (event) => {
+    setState(event.target.value);
   };
 
   useEffect(() => {
@@ -35,54 +37,99 @@ function Home() {
   }, []);
 
   return (
-    <div>
-      <Header />
-      <Container maxWidth="xl" sx={{ mt: 5 }}>
-        <TableContainer component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Id</TableCell>
-                <TableCell align="right">FirstName</TableCell>
-                <TableCell align="right">LastName</TableCell>
-                <TableCell align="right">Email</TableCell>
-                <TableCell align="right">Edit</TableCell>
-                <TableCell align="right">View</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {reduxStore.map((row) => (
-                <TableRow
-                  key={row.id}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.id}
-                  </TableCell>
-                  <TableCell align="right">{row.firstName}</TableCell>
-                  <TableCell align="right">{row.lastName}</TableCell>
-                  <TableCell align="right">{row.email}</TableCell>
-                  <TableCell align="right">
-                  <Link to={`update/${row.id}`}>
-                    <IconButton>
-                      <Edit></Edit>
-                    </IconButton>
-                  </Link>
-                  </TableCell>
-                  <TableCell align="right" onClick={onViewClick}>
-                    <Link to={`profile/${row.id}`}>
-                      <IconButton>
-                        <RemoveRedEyeIcon></RemoveRedEyeIcon>
+    <>
+      <div className="container">
+        <div className="input-container">
+          <TextField
+            id="standard-basic"
+            label="Search Text"
+            variant="standard"
+          />
+        </div>
+        <div className="select-container">
+          <h4>Filter Field</h4>
+          <FormControl sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="demo-simple-select-helper-label">Select</InputLabel>
+            <Select
+              labelId="demo-simple-select-helper-label"
+              id="demo-simple-select-helper"
+              value={state}
+              label="Select"
+              onChange={handleChange}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>First Name(a-z)</MenuItem>
+              <MenuItem value={20}>Last Name(a-z)</MenuItem>
+              <MenuItem value={30}>Email(a-z)</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+        <div className="select-container">
+          <h4>Sort By</h4>
+          <FormControl sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="demo-simple-select-helper-label">Select</InputLabel>
+            <Select
+              labelId="demo-simple-select-helper-label"
+              id="demo-simple-select-helper"
+              value={state}
+              label="Select"
+              onChange={handleChange}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>First Name</MenuItem>
+              <MenuItem value={20}>Last Name</MenuItem>
+              <MenuItem value={30}>Email</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+      </div>
+      {reduxStore.map((row) => (
+        <div key={row.id}>
+          <Grid container spacing={1}>
+            <Grid xs={3}>
+              <div className="card-container">
+                <div className="per-card-1" >
+                  <div className="card-image">
+                    <Avatar
+                      sx={{
+                        bgcolor: `${rgb}`,
+                        width: "150px",
+                        height: "150px",
+                        marginLeft: "32px",
+                      }}
+                    >
+                      {row.firstName.charAt(0)} {row.lastName.charAt(0)}
+                    </Avatar>
+                  </div>
+
+                  <div className="card-content">
+                    <div className="card-title">
+                      {row.firstName} {row.lastName}
+                    </div>
+                    <div className="per-position">{row.email} </div>
+                    <div className="icon-container">
+                      <IconButton onClick={onViewClick}>
+                        <VisibilityIcon />
                       </IconButton>
-                    </Link>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Container>
-    </div>
+                      <IconButton className="icon">
+                        <Edit></Edit>
+                      </IconButton>
+                      <IconButton>
+                        <RestoreFromTrashIcon></RestoreFromTrashIcon>
+                      </IconButton>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Grid>
+          </Grid>
+        </div>
+      ))}
+    </>
   );
 }
 
--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import Avatar from "avataaars";
+import {Avatar} from "@mui/material";
 import "./index.css";
 import { useParams } from "react-router-dom";
 import { getUserDetails } from "../../store/home/HomeAction";
+import Header from "../header/Header";
+
 const ProfilePage = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
@@ -18,43 +20,45 @@ const ProfilePage = () => {
   const reduxStore = useSelector((state) => {
     return state.userDetailReducer.user;
   });
+  const colors = ['#fcba03', '#2596be', '#e28743', '#154c79', '#873e23'];
+  const rgb = colors[Math.floor(Math.random() * colors.length)];
   return (
-    <div className="container">
+    <>
+    <Header/>
+    <div className="profile-container">
       <div className="outer-container">
         <Card sx={{ maxWidth: 650 }}>
           <div className="header-container">
             <div className="image-container">
               <Avatar
-                style={{ width: "100px", height: "100px" }}
-                avatarStyle="Circle"
-                topType="ShortHairShortFlat"
-                accessoriesType="Blank"
-                hairColor="Brown"
-                facialHairType="Blank"
-                clotheType="Hoodie"
-                clotheColor="Blue03"
-                eyeType="Default"
-                eyebrowType="Default"
-                mouthType="Smile"
-                skinColor="Pale"
-              />
+                sx={{
+                  bgcolor: `${rgb}`,
+                  width: "120px",
+                  height: "120px",
+                }}
+              >
+                {reduxStore?.basicInfo?.firstName.charAt(0)} {reduxStore?.basicInfo?.lastName.charAt(0)}
+              </Avatar>
             </div>
             <div className="title-container">
-              <h1>{reduxStore?.basicInfo?.firstName}</h1>
-              <h1>{reduxStore?.basicInfo?.lastName}</h1>
+              <h1 className="title">
+                {reduxStore?.basicInfo?.firstName}{" "}
+                {reduxStore?.basicInfo?.lastName}
+              </h1>
+              <h1></h1>
             </div>
           </div>
           <Card sx={{ minWidth: 275, background: "#f3f0f0", margin: "20px" }}>
             <CardContent>
               <Typography gutterBottom variant="h5" component={"span"}>
-                Personal Information
+              <h5> Personal Information</h5>
               </Typography>
               <Typography
                 variant="body2"
                 color="text.secondary"
                 component={"div"}
               >
-                <h4>First Name: {reduxStore?.basicInfo?.firstName}</h4>
+                <h3>First Name: {reduxStore?.basicInfo?.firstName}</h3>
                 <h4>Last Name: {reduxStore?.basicInfo?.lastName} </h4>
                 <h4>Email: {reduxStore?.basicInfo?.email}</h4>
               </Typography>
@@ -75,7 +79,9 @@ const ProfilePage = () => {
                   School/College/University:{" "}
                   {reduxStore?.academicInfo?.[0]?.institute}
                 </h4>
-                <h4>Passing Year:{reduxStore?.academicInfo?.[0]?.passingYear}</h4>
+                <h4>
+                  Passing Year:{reduxStore?.academicInfo?.[0]?.passingYear}
+                </h4>
               </Typography>
             </CardContent>
           </Card>
@@ -90,18 +96,22 @@ const ProfilePage = () => {
                 component={"div"}
               >
                 <h4>
-                  Employee Code: {reduxStore?.employementInfo?.[0]?.employeeCode}
+                  Employee Code:{" "}
+                  {reduxStore?.employementInfo?.[0]?.employeeCode}
                 </h4>
                 <h4>
                   Company Name: {reduxStore?.employementInfo?.[0]?.companyName}{" "}
                 </h4>
-                <h4>Designation: {reduxStore?.employementInfo?.[0]?.designation} </h4>
+                <h4>
+                  Designation: {reduxStore?.employementInfo?.[0]?.designation}{" "}
+                </h4>
               </Typography>
             </CardContent>
           </Card>
         </Card>
       </div>
     </div>
+    </> 
   );
 };
 
