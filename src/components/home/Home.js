f97c7ac15c5944dec0413a2e04b1291cf4ee8490
AvinashGavandhe <avinash.gavandhe@harbingergroup.com>
2023-03-09 13:03:48 +0530

    cardgrid fixes added

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,14 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Header from "../header/Header";
-import { IconButton, Avatar, TextField, Select, MenuItem, Typography } from "@mui/material";
+import {
+  IconButton,
+  Avatar,
+  TextField,
+  Select,
+  MenuItem,
+  Typography,
+} from "@mui/material";
 import { Edit } from "@mui/icons-material";
 import { users } from "../../store/home/HomeAction";
 import { useNavigate, Link } from "react-router-dom";
@@ -17,14 +24,19 @@ function Home() {
   const dispatch = useDispatch();
   const reduxStore = useSelector((state) => state.usersReducer.users);
   const navigate = useNavigate();
-  
+  console.log(reduxStore);
 
-  const colors = ["red", "green", "blue", "orange", "orange"];
+  const colors = [
+    "blanchedalmond",
+    "chocolate",
+    "darkgray",
+    "darkseagreen",
+    "orange",
+  ];
 
-  const rgb = colors[Math.floor(Math.random() * colors.length)];
-
-  const onViewClick = () => {
-    navigate(`/profile/${reduxStore[0].id}`);
+  const onViewClick = (id) => {
+    console.log(id);
+    navigate(`/profile/${id}`);
   };
   const handleChange = (event) => {
     setState(event.target.value);
@@ -38,7 +50,7 @@ function Home() {
 
   return (
     <>
-    <Header/>
+      <Header />
       <div className="container">
         <div className="input-container">
           <TextField
@@ -49,14 +61,15 @@ function Home() {
           />
         </div>
         <div className="select-container">
-          <Typography  style={{marginLeft:10}}>Filter Field</Typography>
-          <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <Typography style={{ marginLeft: 10 }}>Filter Field</Typography>
+          <FormControl sx={{ m: 1, minWidth: 120, minHeight: 10 }}>
             <InputLabel id="demo-simple-select-helper-label">Select</InputLabel>
             <Select
               labelId="demo-simple-select-helper-label"
               id="demo-simple-select-helper"
               value={state}
               label="Select"
+              style={{ height: 40 }}
               onChange={handleChange}
             >
               <MenuItem value="">
@@ -69,15 +82,16 @@ function Home() {
           </FormControl>
         </div>
         <div className="select-container">
-          <Typography style={{marginLeft:10}}>Sort By</Typography>
+          <Typography style={{ marginLeft: 10 }}>Sort By</Typography>
           <FormControl sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="demo-simple-select-helper-label" >Select</InputLabel>
+            <InputLabel id="demo-simple-select-helper-label">Select</InputLabel>
             <Select
               labelId="demo-simple-select-helper-label"
               id="demo-simple-select-helper"
               value={state}
               label="Select"
               onChange={handleChange}
+              style={{ height: 40 }}
             >
               <MenuItem value="">
                 <em>None</em>
@@ -89,48 +103,48 @@ function Home() {
           </FormControl>
         </div>
       </div>
-      {reduxStore.map((row) => (
-        <div key={row.id}>
-          <Grid container spacing={1}>
-            <Grid xs={3}>
-              <div className="card-container">
-                <div className="per-card-1" >
-                  <div className="card-image">
-                    <Avatar
-                      sx={{
-                        bgcolor: `${rgb}`,
-                        width: "150px",
-                        height: "150px",
-                        marginLeft: "32px",
-                      }}
-                    >
-                      {row.firstName.charAt(0)} {row.lastName.charAt(0)}
-                    </Avatar>
-                  </div>
 
-                  <div className="card-content">
-                    <div className="card-title">
-                      {row.firstName} {row.lastName}
-                    </div>
-                    <div className="per-position">{row.email} </div>
-                    <div className="icon-container">
-                      <IconButton onClick={onViewClick}>
-                        <VisibilityIcon />
-                      </IconButton>
-                      <IconButton className="icon">
-                        <Edit></Edit>
-                      </IconButton>
-                      <IconButton>
-                        <RestoreFromTrashIcon></RestoreFromTrashIcon>
-                      </IconButton>
-                    </div>
+      <Grid container justifyContent="space-between" spacing={1}>
+        {reduxStore.map((row) => (
+          <Grid key={row.id} item>
+            <div className="card-container">
+              <div className="per-card-1">
+                <div className="card-image">
+                  <Avatar
+                    sx={{
+                      bgcolor: `${
+                        colors[Math.floor(Math.random() * colors.length)]
+                      }`,
+                      width: "150px",
+                      height: "150px",
+                      marginLeft: "32px",
+                    }}
+                  >
+                    {row.firstName.charAt(0)} {row.lastName.charAt(0)}
+                  </Avatar>
+                </div>
+                <div className="card-content">
+                  <div className="card-title">
+                    {row.firstName} {row.lastName}
+                  </div>
+                  <div className="per-position">{row.email} </div>
+                  <div className="icon-container">
+                    <IconButton onClick={() =>onViewClick(row.id)}>
+                      <VisibilityIcon />
+                    </IconButton>
+                    <IconButton className="icon">
+                      <Edit></Edit>
+                    </IconButton>
+                    <IconButton>
+                      <RestoreFromTrashIcon></RestoreFromTrashIcon>
+                    </IconButton>
                   </div>
                 </div>
               </div>
-            </Grid>
+            </div>
           </Grid>
-        </div>
-      ))}
+        ))}
+      </Grid>
     </>
   );
 }
